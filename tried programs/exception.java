b55fbfb19240b8b106075347e70213d46688c113
santhiyaramaraj <34671089+santhiyaramaraj@users.noreply.github.com>
2018-02-22 09:29:12 +0530

    Create exception.java

---
+++ tried programs/exception.java
@@ -0,0 +1,23 @@
+//Try to execute the statement "mandate"
+
+
+
+package exception;
+
+public class Allocate {
+			  public static void main(String[] args) {
+		 
+		    try {
+		      long data[] = new long[10000000]; 
+		    }
+		    catch (Exception e) {
+		      System.out.println(e);
+		    }
+		 
+		    
+		      System.out.println("mandate");
+		    
+		  }
+		}
+
+
